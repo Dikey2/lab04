842ffd85eec9ea7b5ce52e5786d4f4c0f19d356c
Alexis Villar <alexis.villar@tecsup.edu.pe>
2026-04-10 17:33:10 -0500

    feat: add Card component

--- MovieCounter/app/src/main/java/com/example/moviecounter/MainActivity.kt
+++ MovieCounter/app/src/main/java/com/example/moviecounter/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.moviecounter.ui.theme.MovieCounterTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MovieCounterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MiCard(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +30,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MiCard(modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.padding(16.dp)
+    ) {
+        Text(
+            text = "Esta es una Card",
+            modifier = Modifier.padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MiCardPreview() {
     MovieCounterTheme {
-        Greeting("Android")
+        MiCard()
     }
 }
